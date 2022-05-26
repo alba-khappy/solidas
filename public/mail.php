3df4965f1c65750345610828796a861e06da02c6
albina <albinabist1993@mail.ru>
2022-05-26 23:36:55 +0300

    fix email

--- public/mail.php
+++ public/mail.php
@@ -2,7 +2,7 @@
   $json = file_get_contents('php://input');
   $data = json_decode($json);
   $headers = "From: order@solidas.ru";
-  if (mail('albinabist1993@mail.ru', 'Новое обращение', 'Сообщение от' . $data->name . ' ' . 'Номер телефона' . $data->phone . ' '. ' Email отправителя ' . $data->email, $headers)) {
+  if (mail('pierroit@yandex.ru', 'Новое обращение', 'Сообщение от' . $data->name . ' ' . 'Номер телефона' . $data->phone . ' '. ' Email отправителя ' . $data->email, $headers)) {
     http_response_code(200);
   } else {
     http_response_code(500);
